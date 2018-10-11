6b2f29d913eb2862380748635b4cfff5ff3ed72d
unknown <alvislee2016@gmail.com>
2018-10-11 20:43:28 +0800

    Setup redis and basic get and set state in server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API works");
+// Set up Redis Client
+const redisClient = require('redis').createClient(process.env.REDIS_URL);
+redisClient.on('connect', () => console.log('Redis client connected'));
+redisClient.on('error', err => console.log('Redis client error: ' + err));
+redisClient.set('gameState', 'gameStateObject');
+
+app.get('/getGameState', (req, res) => {
+  redisClient.get('gameState', function (error, result) {
+    res.send(result);
+  })
 });
 
 const PORT = process.env.PORT || 5000;
