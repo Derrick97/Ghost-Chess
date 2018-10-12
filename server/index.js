26ec08985522d2d3659643a32fffe676d8aa150d
unknown <alvislee2016@gmail.com>
2018-10-13 05:32:23 +0800

    Debug Redis

--- server/index.js
+++ server/index.js
@@ -1,97 +1,96 @@
 const express = require("express");
 const BodyParser = require("body-parser");
-const Redis = require('ioredis');
 const app = express();
 app.use(BodyParser.urlencoded({ extended: false }))
 app.use(BodyParser.json())
 
-const redis = new Redis(process.env.REDIS_URL);
-const gameState = [
-    {col: 0, row: 0, piece: null},
-    {col: 1, row: 0, piece: null},
-    {col: 2, row: 0, piece: null},
-    {col: 3, row: 0, piece: null},
-    {col: 4, row: 0, piece: {type: 'P', color: 'white'}},
-    {col: 5, row: 0, piece: null},
-    {col: 6, row: 0, piece: null},
-    {col: 7, row: 0, piece: null},
-    {col: 0, row: 1, piece: null},
-    {col: 1, row: 1, piece: null},
-    {col: 2, row: 1, piece: null},
-    {col: 3, row: 1, piece: null},
-    {col: 4, row: 1, piece: null},
-    {col: 5, row: 1, piece: null},
-    {col: 6, row: 1, piece: null},
-    {col: 7, row: 1, piece: null},
-    {col: 0, row: 2, piece: null},
-    {col: 1, row: 2, piece: null},
-    {col: 2, row: 2, piece: null},
-    {col: 3, row: 2, piece: null},
-    {col: 4, row: 2, piece: null},
-    {col: 5, row: 2, piece: null},
-    {col: 6, row: 2, piece: null},
-    {col: 7, row: 2, piece: null},
-    {col: 0, row: 3, piece: null},
-    {col: 1, row: 3, piece: null},
-    {col: 2, row: 3, piece: null},
-    {col: 3, row: 3, piece: null},
-    {col: 4, row: 3, piece: null},
-    {col: 5, row: 3, piece: null},
-    {col: 6, row: 3, piece: null},
-    {col: 7, row: 3, piece: null},
-    {col: 0, row: 4, piece: null},
-    {col: 1, row: 4, piece: null},
-    {col: 2, row: 4, piece: null},
-    {col: 3, row: 4, piece: null},
-    {col: 4, row: 4, piece: null},
-    {col: 5, row: 4, piece: null},
-    {col: 6, row: 4, piece: null},
-    {col: 7, row: 4, piece: null},
-    {col: 0, row: 5, piece: null},
-    {col: 1, row: 5, piece: null},
-    {col: 2, row: 5, piece: null},
-    {col: 3, row: 5, piece: null},
-    {col: 4, row: 5, piece: null},
-    {col: 5, row: 5, piece: null},
-    {col: 6, row: 5, piece: null},
-    {col: 7, row: 5, piece: null},
-    {col: 0, row: 6, piece: null},
-    {col: 1, row: 6, piece: null},
-    {col: 2, row: 6, piece: null},
-    {col: 3, row: 6, piece: null},
-    {col: 4, row: 6, piece: null},
-    {col: 5, row: 6, piece: null},
-    {col: 6, row: 6, piece: null},
-    {col: 7, row: 6, piece: null},
-    {col: 0, row: 7, piece: null},
-    {col: 1, row: 7, piece: null},
-    {col: 2, row: 7, piece: null},
-    {col: 3, row: 7, piece: null},
-    {col: 4, row: 7, piece: null},
-    {col: 5, row: 7, piece: null},
-    {col: 6, row: 7, piece: null},
-    {col: 7, row: 7, piece: null},
-];
-
 // Set up Redis Client
-async function setupGameState() {
-    try {
-        const result = await redis.sadd('gameState', gameState);
-        console.log(result);
-    } catch (e) {
-        console.error(e);
-    }
-}
-setupGameState();
+const redisClient = require('redis').createClient(process.env.REDIS_URL);
+redisClient.on('connect', () => console.log('Redis client connected'));
+redisClient.on('error', err => console.log('Redis client error: ' + err));
+redisClient.flushall(function (err, res) {
+  const gameState = [
+    { col: 0, row: 0, piece: null },
+    { col: 1, row: 0, piece: null },
+    { col: 2, row: 0, piece: null },
+    { col: 3, row: 0, piece: null },
+    { col: 4, row: 0, piece: { type: 'P', color: 'white' } },
+    { col: 5, row: 0, piece: null },
+    { col: 6, row: 0, piece: null },
+    { col: 7, row: 0, piece: null },
+    { col: 0, row: 1, piece: null },
+    { col: 1, row: 1, piece: null },
+    { col: 2, row: 1, piece: null },
+    { col: 3, row: 1, piece: null },
+    { col: 4, row: 1, piece: null },
+    { col: 5, row: 1, piece: null },
+    { col: 6, row: 1, piece: null },
+    { col: 7, row: 1, piece: null },
+    { col: 0, row: 2, piece: null },
+    { col: 1, row: 2, piece: null },
+    { col: 2, row: 2, piece: null },
+    { col: 3, row: 2, piece: null },
+    { col: 4, row: 2, piece: null },
+    { col: 5, row: 2, piece: null },
+    { col: 6, row: 2, piece: null },
+    { col: 7, row: 2, piece: null },
+    { col: 0, row: 3, piece: null },
+    { col: 1, row: 3, piece: null },
+    { col: 2, row: 3, piece: null },
+    { col: 3, row: 3, piece: null },
+    { col: 4, row: 3, piece: null },
+    { col: 5, row: 3, piece: null },
+    { col: 6, row: 3, piece: null },
+    { col: 7, row: 3, piece: null },
+    { col: 0, row: 4, piece: null },
+    { col: 1, row: 4, piece: null },
+    { col: 2, row: 4, piece: null },
+    { col: 3, row: 4, piece: null },
+    { col: 4, row: 4, piece: null },
+    { col: 5, row: 4, piece: null },
+    { col: 6, row: 4, piece: null },
+    { col: 7, row: 4, piece: null },
+    { col: 0, row: 5, piece: null },
+    { col: 1, row: 5, piece: null },
+    { col: 2, row: 5, piece: null },
+    { col: 3, row: 5, piece: null },
+    { col: 4, row: 5, piece: null },
+    { col: 5, row: 5, piece: null },
+    { col: 6, row: 5, piece: null },
+    { col: 7, row: 5, piece: null },
+    { col: 0, row: 6, piece: null },
+    { col: 1, row: 6, piece: null },
+    { col: 2, row: 6, piece: null },
+    { col: 3, row: 6, piece: null },
+    { col: 4, row: 6, piece: null },
+    { col: 5, row: 6, piece: null },
+    { col: 6, row: 6, piece: null },
+    { col: 7, row: 6, piece: null },
+    { col: 0, row: 7, piece: null },
+    { col: 1, row: 7, piece: null },
+    { col: 2, row: 7, piece: null },
+    { col: 3, row: 7, piece: null },
+    { col: 4, row: 7, piece: null },
+    { col: 5, row: 7, piece: null },
+    { col: 6, row: 7, piece: null },
+    { col: 7, row: 7, piece: null },
+  ];
+  redisClient.sadd("gameState", gameState, function (err, res) {
+    console.log(res);
+  })
+});
+
 
 app.get('/', (req, res) => {
   res.send('API working');
 });
 
 // Get game state of chessboard
-app.get('/getGameState', async (req, res) => {
-    const gameState = await getGameState();
-    res.json(gameState);
+app.get('/getGameState', (req, res) => {
+  redisClient.smembers('gameState', function (err, reply) {
+    console.log(resply);
+    res.json(reply);
+  });
 });
 
 // Make a move and update chessboard
@@ -103,15 +102,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
   console.log(`App listening on port ${PORT}`);
 });
-
-
-async function getGameState() {
-    try {
-        redis.smembers('gameState').then(function(result) {
-            console.log(result);
-            return result;
-        });
-    } catch (error) {
-        console.log(error);
-    }
-}
