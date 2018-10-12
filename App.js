cb7b4ea139510bce182fc8b50a2d536ad74a6699
TerrenceCKCHAN <ckterrencechan@gmail.com>
2018-10-12 17:47:15 +0100

    Add connection to ChessBoard

--- App.js
+++ App.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import ChessBoard from './ChessBoard';
 
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = { text: 'Open up App.js to start working on your app!' };
+    this.state = { text: 'Open x App.js to start working on your app!' };
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>{this.state.text}</Text>
-
-        {/* Example of calling API using node fetch */}
-        <Button title="Get GameState" onPress={() => {
-          fetch('https://ghost-chess.herokuapp.com/getGameState')
-            .then(res => res.text())
-            .then(text => this.setState({ text: text }));
-        }} />
-
-      </View>
+       <ChessBoard/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+        // {/* Example of calling API using node fetch */}
+        // <Button title="Get GameState" onPress={() => {
+        //   fetch('https://ghost-chess.herokuapp.com/getGameState')
+        //     .then(res => res.text())
+        //     .then(text => this.setState({ text: text }));
+        // }} />
