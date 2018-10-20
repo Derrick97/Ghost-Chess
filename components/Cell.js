18861c899f9d98c40e548865e0f8825fcc5d9786
TerrenceCKCHAN <ckterrencechan@gmail.com>
2018-10-20 23:36:55 +0100

    Implement renderChessPiece to display chess pieces from character code

--- components/Cell.js
+++ components/Cell.js
@@ -26,13 +26,41 @@ export default class Cell extends React.Component {
     this.props.handleCellPress(this.props.id)
   }
 
+  renderChessPiece() {
+    let chessCode;
+    if (this.props.piece.color === 'white') {
+      chessCode = 0;
+    }
+    switch (this.props.piece.type) {
+      case 'K':
+        chessCode += 9812;
+        break;
+      case 'Q':
+        chessCode += 9813;
+        break;
+      case 'R': 
+        chessCode += 9814;
+        break;
+      case 'B': 
+        chessCode += 9815;
+        break;
+      case 'H': 
+        chessCode += 9816;
+        break;
+      case 'P': 
+        chessCode += 9817;
+        break;
+    }
+    return chessCode;
+  }
+
   render() {
     return (
       <StyledCell underlayColor='grey' col={this.props.col} row={this.props.row} onPress={this.handlePress}>
         {
           this.props.piece ?
             <Text style={{ color: this.props.piece.color }}>
-              {String.fromCharCode(9812)}
+              {String.fromCharCode(this.renderChessPiece)}
             </Text> :
             <Text/>
         }
