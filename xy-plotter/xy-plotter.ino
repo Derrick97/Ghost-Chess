afd051bd9ec071428274e23f3efee9c23c798e19
Ki <ki.cheuk16@imperial.ac.uk>
2018-10-29 13:29:44 +0000

    draft for controlling magnet and basic instruct

--- xy-plotter/xy-plotter.ino
+++ xy-plotter/xy-plotter.ino
@@ -9,63 +9,71 @@ double angle_deg = 180.0/PI;
 MeSerial se;
 MeStepper stepper_1(1);
 MeStepper stepper_2(2);
+int direction;
+int distance;
+boolean magnet =false;
+double MaxSpeed = 100.0;
+double Speed = 100.0;
+double CellSize = 30.0;
+MeLimitSwitch sw_3_1(3,1);
+MeLimitSwitch sw_3_2(3,2);
+MeLimitSwitch sw_6_1(6,1);
+MeLimitSwitch sw_6_2(6,2);
 
 
 
 void setup(){
-    Serial.begin(115200);
+    Serial.begin(9600);
     Serial.println("Connected");
+    pinMode(6, OUTPUT); 
     
 }
 
 void loop(){
     
     while(Serial.available()){
-        /*
-        char buffer_[20];
-        Serial.readBytes(buffer_, 20);
-        Serial.println(buffer_);
-        */
-        
-        char command = (char)Serial.read();
-        
-        if(command == 'x'){
-            stepper_1.move(60);
-            stepper_1.setMaxSpeed(100);
-            stepper_1.setSpeed(100);
+        char command[3];
+        Serial.readBytes(command,3);
+        Serial.println(command);
+
+        if ((String)command == "000") {
         }
-        if(command == 'y'){
-            stepper_2.move(60);
-            stepper_2.setMaxSpeed(100);
-            stepper_2.setSpeed(100);
+        else{
+        direction = command[0];
+        distance = command[1];
+        magnet = command[2];
+        int x;
+        int y;
+//        switch (direction) {
+//          case 48:
+//          break;
+//          break;
+//          default:
+//          break;
+//        }
+//        if (magnet){
+//          digitalWrite(6, HIGH);
+//          }
+//      stepper_1.move(CellSize);
+//      stepper_1.setMaxSpeed(MaxSpeed);
+//      stepper_1.setSpeed(Speed);
+//      stepper_2.move(CellSize);
+//      stepper_2.setMaxSpeed(MaxSpeed);
+//      stepper_2.setSpeed(Speed);
+//         }
         }
-        if(command == 'w'){
-            stepper_1.move(60);
-            stepper_2.move(60);
-            stepper_2.setMaxSpeed(100);
-            stepper_2.setSpeed(100);
-        }
-        if(command == 'z'){
-            stepper_1.move(-60);
-            stepper_2.move(-60);
-            stepper_2.setMaxSpeed(100);
-            stepper_2.setSpeed(100);
-        }
-        
-    }
-    
-    _loop();
+//    
+//    _loop(magnet);
+}
 }
 
 void _delay(float seconds){
     long endTime = millis() + seconds * 1000;
-    while(millis() < endTime)_loop();
+    while(millis() < endTime)_loop(magnet);
 }
 
-void _loop(){
-    stepper_1.runSpeedToPosition();
-    
-    stepper_2.runSpeedToPosition();
-    
-    
+void _loop(int magnet){
+  if(!stepper_1.runSpeedToPosition() && !stepper_2.runSpeedToPosition() && magnet == true){
+    digitalWrite(6,LOW);
+    }
 }
