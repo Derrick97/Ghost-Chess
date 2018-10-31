914d1d4dd42c957e4258f93ada22cd294f4d95c0
Ki <ki.cheuk16@imperial.ac.uk>
2018-10-31 15:10:31 +0000

    implement direction and distance for plotter

--- xy-plotter/xy-plotter.ino
+++ xy-plotter/xy-plotter.ino
@@ -4,14 +4,19 @@
 
 #include <MeOrion.h>
 
+#define CommandsSize 25
+
+struct command {
+  int direction = 4;
+  int distance = 0;
+  boolean magnet =false;
+}commands[25];
+
 double angle_rad = PI/180.0;
 double angle_deg = 180.0/PI;
 MeSerial se;
 MeStepper stepper_1(1);
 MeStepper stepper_2(2);
-int direction;
-int distance;
-boolean magnet =false;
 double MaxSpeed = 100.0;
 double Speed = 100.0;
 double CellSize = 30.0;
@@ -19,61 +24,110 @@ MeLimitSwitch sw_3_1(3,1);
 MeLimitSwitch sw_3_2(3,2);
 MeLimitSwitch sw_6_1(6,1);
 MeLimitSwitch sw_6_2(6,2);
+char buffer[4];
+// index: how many command in commands
+int WriteIndex=0;
+int ReadIndex=0;
 
 
 
 void setup(){
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println("Connected");
     pinMode(6, OUTPUT); 
-    
 }
 
 void loop(){
-    
-    while(Serial.available()){
-        char command[3];
-        Serial.readBytes(command,3);
-        Serial.println(command);
-
-        if ((String)command == "000") {
+      while(Serial.available()){
+        delay(1000);
+        if(Serial.available()>=4){
+          Serial.println("wp 1");
+          Serial.readBytes(buffer,4);
+          char term = buffer[3];
+          if (term=='#'){
+          commands[WriteIndex].direction = buffer[0]-48;
+          commands[WriteIndex].distance = buffer[1]-48;
+          commands[WriteIndex].magnet = buffer[2]-48;
+          Serial.println(commands[WriteIndex].distance);
+//          Serial.read();
+          WriteIndex++;
+          Serial.write(buffer,3);
+          Serial.println(Serial.available());
+          Serial.println(WriteIndex);
+           
         }
         else{
-        direction = command[0];
-        distance = command[1];
-        magnet = command[2];
-        int x;
-        int y;
-//        switch (direction) {
-//          case 48:
-//          break;
-//          break;
-//          default:
-//          break;
-//        }
-//        if (magnet){
-//          digitalWrite(6, HIGH);
-//          }
-//      stepper_1.move(CellSize);
-//      stepper_1.setMaxSpeed(MaxSpeed);
-//      stepper_1.setSpeed(Speed);
-//      stepper_2.move(CellSize);
-//      stepper_2.setMaxSpeed(MaxSpeed);
-//      stepper_2.setSpeed(Speed);
-//         }
+          Serial.write("Error terminator\n");
+          }
         }
-//    
-//    _loop(magnet);
+        else{
+          while(Serial.available()){
+           Serial.read();
+          }
+        
+        }
+        //clearing serial data
+//        else{
+//          Serial.print(Serial.available());
+          
+//        }
+  }
+        
+      if(commands[ReadIndex].distance!=0){
+       Serial.println("wp2");
+       Serial.println(commands[ReadIndex].direction);
+       Serial.println(commands[ReadIndex].distance);
+       Serial.println(commands[ReadIndex].magnet);
+       int x = 0;
+       int y = 0;
+       switch (commands[ReadIndex].direction) {
+          case 0: y = commands[ReadIndex].distance;          
+          break;
+          case 1: y = commands[ReadIndex].distance;
+                   x = commands[ReadIndex].distance;
+          break;
+          case 2: x = commands[ReadIndex].distance;
+          break;
+          case 3: y = -1 * commands[ReadIndex].distance;
+                 x = commands[ReadIndex].distance;
+          case 4: y = -1 * commands[ReadIndex].distance;
+          break;
+          case 5: y = -1* commands[ReadIndex].distance;
+                   x = -1* commands[ReadIndex].distance;
+          break;
+           case 6: x = -1* commands[ReadIndex].distance;
+          break;
+           case 7: y = commands[ReadIndex].distance;
+                   x = -1* commands[ReadIndex].distance;
+          break;
+          default:
+           break;
 }
+      Serial.println("x:");
+      Serial.println(x);
+      Serial.println("y:");
+      Serial.println(y);
+      stepper_1.move(CellSize*x);
+      stepper_1.setMaxSpeed(MaxSpeed);
+      stepper_1.setSpeed(Speed);
+      stepper_2.move(CellSize*y);
+      stepper_2.setMaxSpeed(MaxSpeed);
+      stepper_2.setSpeed(Speed);
+      ReadIndex++;
+      }  
+     _loop();
+//}
 }
 
 void _delay(float seconds){
     long endTime = millis() + seconds * 1000;
-    while(millis() < endTime)_loop(magnet);
+    while(millis() < endTime)_loop();
 }
 
-void _loop(int magnet){
-  if(!stepper_1.runSpeedToPosition() && !stepper_2.runSpeedToPosition() && magnet == true){
-    digitalWrite(6,LOW);
-    }
+void _loop(){
+//  if(!stepper_1.runSpeedToPosition() && !stepper_2.runSpeedToPosition() && magnet == true){
+//    digitalWrite(6,LOW);
+//    }
+stepper_1.runSpeedToPosition();
+stepper_2.runSpeedToPosition();
 }
